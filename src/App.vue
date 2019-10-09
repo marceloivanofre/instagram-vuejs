82f87679ab0720af34093902e73c8f0a0eb70894
marceloivanofre <marceloivanofre13@gmail.com>
2019-10-09 11:54:31 -0300

    refactor: scss external file

--- src/App.vue
+++ src/App.vue
@@ -5,26 +5,5 @@
 </template>
 
 <style lang="scss">
-html,
-body,
-#app {
-  width: 100%;
-  height: 100%;
-}
-
-body {
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-  background: #fafafa;
-  font-family: "Roboto", Helvetica, sans-serif;
-}
-
-a {
-  text-decoration: none;
-}
-
-li {
-  list-style: none;
-}
+@import "./assets/scss/main.scss";
 </style>
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="login-page">
+  <div class="register-page">
     <img src="../assets/instagram-word-logo.png" />
     <h2>Cadastre-se para ver fotos e vídeos dos seus amigos.</h2>
     <form class="form-group" @submit.prevent="register">
@@ -61,86 +61,3 @@ export default {
   }
 };
 </script>
-
-<style lang="scss" scoped>
-.login-page {
-  display: flex;
-  flex-flow: column;
-  justify-content: center;
-  min-height: 100%;
-  padding: 25px;
-
-  img {
-    height: 60px;
-    object-fit: contain;
-    margin-bottom: 10px;
-  }
-
-  h2 {
-    font-size: 17px;
-    text-align: center;
-    font-weight: bold;
-    color: #999;
-    margin-bottom: 20px;
-  }
-
-  div {
-    display: flex;
-    justify-content: space-around;
-    font-size: 14px;
-    font-weight: bold;
-    color: #0008;
-    margin-bottom: 20px;
-
-    hr {
-      width: 42%;
-      border: 0.2px solid #ddd;
-    }
-  }
-
-  span {
-    font-size: 14px;
-    text-align: center;
-    color: #262626;
-
-    a {
-      font-weight: bold;
-      color: #3897f7;
-    }
-  }
-
-  .form-group {
-    display: flex;
-    flex-flow: column;
-    margin-bottom: 25px;
-
-    input {
-      border: 1px solid #ddd;
-      max-height: 36px;
-      border-radius: 4px;
-      margin-bottom: 15px;
-      padding: 9px 0 7px 10px;
-      background: #fafafa;
-      outline: none;
-
-      &:focus {
-        border: 1px solid #aaa;
-      }
-    }
-
-    button {
-      width: 100%;
-      height: 30px;
-      appearance: none;
-      border: none;
-      outline: none;
-      border-radius: 3px;
-      background-color: #3897f7;
-      color: #fff;
-      font-size: 16px;
-      font-weight: 700;
-      cursor: pointer;
-    }
-  }
-}
-</style>
